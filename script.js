78789d0c2f4c5bb0fb72d0a77f924c3889e0665c
eng.neda <eng.pourandish@gmail.com>
2025-09-28 19:13:02 +0330

    login

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const form = document.querySelector("#loginForm");
-const formData = new FormData(form);
-const formValues = {
-  email: formData.get("email"),
-  password: formData.get("password"),
-};
-console.log(formValues);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = document.querySelector("#loginForm");
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const payload = {
+    email,
+    password,
+  };
+
+  if (!payload.email || !payload.password) {
+    alert("لطفاً ایمیل و رمز عبور را وارد کنید.");
+    return;
+  }
+
+  fetch("https://karyar-library-management-system.liara.run/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log("data:", data))
+    .catch((err) => console.error("Error:", err));
+}
+document.querySelector("#loginForm").addEventListener("submit", handleSubmit);
