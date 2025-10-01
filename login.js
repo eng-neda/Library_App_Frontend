1602cf2acee5b90e4910dee3663b2112dee16e3a
eng.neda <eng.pourandish@gmail.com>
2025-10-01 12:30:39 +0330

    dashboard.js added

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
+////loging
 let isSubmit = false;
 const overlay = document.getElementById("coverage");
 const loginBtn = document.getElementById("loginBtn");
+const form = document.getElementById("loginForm");
 
 function handleSubmit(event) {
   event.preventDefault();
   if (isSubmit) return;
   isSubmit = true;
 
-  const form = document.querySelector("#loginForm");
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
 
@@ -37,16 +38,23 @@ function handleSubmit(event) {
     .then((data) => {
       console.log("ورود با موفقیت انجام شد:", data);
       if (data && data.token) {
-        document.cookie =
-          "token=${data.token}; path=/; max-age=3600; secure; samesite=strict";
+        document.cookie = `token=${data.token}; path=/; max-age=86400; secure; samesite=strict`;
         console.log("توکن در کوکی ذخیره شد");
-      }
+
+        if (data && data.user) {
+          localStorage.setItem("userName", data.user.firstName);
+        }
+        console.log(localStorage.getItem("userName"));
+        window.location.href = "dashboard.html";
+      } else window.location.href = "login.html";
     })
     .catch((err) => {
       if (err.message.includes("Failed to fetch")) {
         console.error("خطای شبکه: لطفاً اتصال اینترنت خود را بررسی کنید");
+        alert(`Error: ${err.message}`);
       } else {
-        console.error("خطای JSON:", err.message);
+        console.error("خطا:", err.message);
+        alert(`Error: ${err.message}`);
       }
     })
     .finally(() => {
@@ -55,5 +63,6 @@ function handleSubmit(event) {
       isSubmit = false;
     });
 }
-
 document.querySelector("#loginForm").addEventListener("submit", handleSubmit);
+
+//نشان دادن پیغام ها به کاربر
